ce7bf6c48795e2337fa0f1372e3a3f500e5944b0
lzil <zhoul@mit.edu>
2017-11-06 12:34:50 -0500

    save and go to timestamp

--- yt-dance.js
+++ yt-dance.js
@@ -4,10 +4,13 @@ speed / mirroring tools for Youtube
 
 (useful for learning dance from videos)
 
-- press 'p' to speed up by 0.1x
-- press 'q' to slow down by 0.1x
-- press 'i' to mirror (and again to unmirror)
-- press 'r' to reset to 1x unmirrored (original video)
+- 'p' to speed up by 0.1x
+- 'q' to slow down by 0.1x
+- 'i' to mirror (and again to unmirror)
+- 'r' to reset to 1x unmirrored (original video)
+
+- 's' to save current timestamp
+- 'g' to go to saved timestamp
 
 */
 
@@ -21,34 +24,64 @@ var yt_dance_main = function() {
     }
 
     var vid_buttons = document.getElementsByClassName('ytp-chrome-top-buttons')[0];
-    var node = document.createTextNode("[1x]");
-    var para = document.createElement("p");
-    para.appendChild(node);
-    vid_buttons.appendChild(para);
-    para.setAttribute('id', 'video-speed-label')
-    para.style.backgroundColor = 'rgba(0, 0, 0, 0.3)';
-    para.style.fontSize = '25px';
-    para.style.marginTop = '10px';
+    var speed_node = document.createTextNode("[1x]");
+    var speed_text = document.createElement("p");
+    speed_text.appendChild(speed_node);
+    vid_buttons.appendChild(speed_text);
+    speed_text.setAttribute('id', 'video-speed-label')
+    speed_text.style.backgroundColor = 'rgba(0, 0, 0, 0.3)';
+    speed_text.style.fontSize = '25px';
+    speed_text.style.marginTop = '10px';
+    speed_text.style.textAlign = 'right'
+
+    var time_node = document.createTextNode("0:00:00");
+    var time_text = document.createElement("p");
+    time_text.appendChild(time_node);
+    vid_buttons.appendChild(time_text);
+    time_text.setAttribute('id', 'video-time-label')
+    time_text.style.backgroundColor = 'rgba(0, 0, 0, 0.3)';
+    time_text.style.fontSize = '25px';
+    time_text.style.textAlign = 'right'
     //para.style.marginRight = '15px';
 
-    var video = document.getElementsByTagName('video')[0]
+    var video = document.getElementsByTagName('video')[0];
+    var player = document.getElementById('movie_player');
     var mirrored = false;
-    console.log('done')
+    var time_save = 0;
 
     window.onkeydown = function(event) {
         var c = video.playbackRate;
+        //var c = player.getPlaybackRate();
         switch (event.keyCode) {
-            case 73:
+            case 83: //s
+                time_save = player.getCurrentTime();
+                var mins = parseInt(time_save / 60)
+                var secs = parseInt(time_save - mins * 60)
+                var msecs = parseInt(100*(time_save - mins * 60 - secs))
+
+                if (secs < 10) {
+                    secs = '0' + secs;
+                }
+                if (msecs < 10) {
+                    msecs = '0' + msecs;
+                }
+                time_text.textContent = mins + ':' + secs + ':' + msecs;
+                break;
+            case 71: //g
+                player.seekTo(time_save, true);
+                break;  
+            case 73: //i
                 mirrored = !mirrored;
                 flip();
                 break;
-            case 80:
+            case 80: //p
                 c += 0.1;
+                console.log(c)
                 break;
-            case 81:
+            case 81: //q
                 c -= 0.1;
                 break;
-            case 82:
+            case 82: //r
                 c = 1;
                 if (mirrored) {
                     flip();
@@ -57,6 +90,7 @@ var yt_dance_main = function() {
                 break;
         }
         video.playbackRate = c;
+        //player.setPlaybackRate(c);
         var str = ' [' + Math.round(c * 10)/10 + 'x';
         if (mirrored) {
             str += ', mirrored'
@@ -65,9 +99,9 @@ var yt_dance_main = function() {
         // for (t = 0; t < titles.length; t++) {
         //     titles[t].textContent = titles_orig[t] + str;
         // }
-        para.textContent = str;
-        
-        console.log('playback rate:',video.playbackRate)
+        speed_text.textContent = str;
+        console.log('timestamp:',video.playbackRate)
+        console.log('playback rate:',player.getPlaybackRate())
         console.log('mirrored:', mirrored)
     };
 
