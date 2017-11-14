83b98c1abf198312744329d72b617134b1eea655
lzil <zhoul@mit.edu>
2017-11-14 00:23:41 -0500

    analytics

--- yt-dance.js
+++ yt-dance.js
@@ -224,3 +224,11 @@ try {
     yt_dance_main();
 }
 
+//some google analytics so i can see how the script is being used lol
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+ga('create', 'UA-65259513-3', 'auto');
+ga('send', 'pageview');
